095ec66dea8e11b58243057a0dc936ad9a143f92
unknown <maxim.malashev@gmail.com>
2020-11-16 23:05:41 +0100

    added buffers and shader abstraction

---
+++ src/engine/rendering/shader.cpp
@@ -0,0 +1,153 @@
+#include <GL/glew.h>
+
+#include <iostream>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include <fstream>
+
+#include "shader.h"
+
+Shader* Shader::load(const char* vertexPath, const char* fragmentPath)
+{
+	Shader* shader = new Shader();
+
+	std::string vertex = readFileAsString(vertexPath);
+	std::string fragment = readFileAsString(fragmentPath);
+
+	shader->compile(vertex, fragment);
+
+	return shader;
+}
+
+void Shader::use()
+{
+	glUseProgram(id);
+}
+
+void Shader::clear()
+{
+	if (id != 0)
+	{
+		glDeleteProgram(id);
+		id = 0;
+	}
+}
+
+void Shader::setUniform1f(std::string location, float value)
+{
+	use();
+	int locationId = glGetUniformLocation(id, location.c_str());
+	glUniform1f(locationId, value);
+}
+
+void Shader::setUniformMat4(std::string location, glm::mat4 value)
+{
+	use();
+	int locationId = glGetUniformLocation(id, location.c_str());
+	glUniformMatrix4fv(locationId, 1, GL_FALSE, glm::value_ptr(value));	
+}
+
+void Shader::setMVP(glm::mat4 model, glm::mat4 view, glm::mat4 projection)
+{
+	setUniformMat4("model", model);
+	setUniformMat4("view", view);
+	setUniformMat4("projection", projection);
+}
+
+void Shader::compile(std::string vertexCode, std::string fragmentCode)
+{
+	id = glCreateProgram();
+
+	if (!id)
+	{
+		std::cout << "Error creating shader program." << std::endl;
+		return;
+	}
+
+	add(vertexCode.c_str(), GL_VERTEX_SHADER);
+	add(fragmentCode.c_str(), GL_FRAGMENT_SHADER);
+
+	glLinkProgram(id);
+	if (!checkProgramStatus(GL_LINK_STATUS)) { std::exit(-1); }
+
+	glValidateProgram(id);
+	if (!checkProgramStatus(GL_VALIDATE_STATUS)) { std::exit(-1); }
+}
+
+void Shader::add(const char* shaderCode, GLenum type)
+{
+	unsigned int shader = glCreateShader(type);
+
+	const char* code[1];
+	code[0] = shaderCode;
+
+	int codeLength[1];
+	codeLength[0] = strlen(shaderCode);
+
+	glShaderSource(shader, 1, code, codeLength);
+	glCompileShader(shader);
+
+	glLinkProgram(shader);
+	if (!checkShaderStatus(shader, GL_COMPILE_STATUS)) { std::exit(-1); }
+
+	glAttachShader(id, shader);
+}
+
+bool Shader::checkProgramStatus(GLenum status)
+{
+	int result = 0;
+	char eLog[1024] = { 0 };
+
+	glGetProgramiv(id, status, &result);
+
+	if (!result)
+	{
+		glGetProgramInfoLog(id, sizeof(eLog), nullptr, eLog);
+		std::cout << "Error checking program status: " << eLog << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+bool Shader::checkShaderStatus(unsigned int shader, GLenum status)
+{
+	int result = 0;
+	char eLog[1024] = { 0 };
+
+	glGetShaderiv(shader, status, &result);
+
+	if (!result)
+	{
+		glGetShaderInfoLog(shader, sizeof(eLog), nullptr, eLog);
+		std::cout << "Error checking shader status: " << eLog << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+std::string Shader::readFileAsString(const char* path)
+{
+	std::string content;
+	std::ifstream fileStream(path, std::ios::in);
+
+	if (!fileStream.is_open())
+	{
+		std::cout << "File doesn't exist: " << path << std::endl;
+		std::exit(-1);
+	}
+
+	std::string line = "";
+	while (!fileStream.eof())
+	{
+		std::getline(fileStream, line);
+		content.append(line + "\n");
+	}
+
+	fileStream.close();
+	return content;
+}
+
+Shader::Shader() : id(0) { }
+Shader::~Shader() { clear(); }
---
+++ src/engine/rendering/shader.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <GL/glew.h>
+
+#include <string>
+#include <glm/mat4x4.hpp>
+
+class Shader
+{
+public:
+	~Shader();
+
+	static Shader* load(const char* vertexPath, const char* fragmentPath);
+
+	void use();
+	void clear();
+
+	void setUniform1f(std::string location, float value);
+	void setUniformMat4(std::string location, glm::mat4 value);
+
+	void setMVP(glm::mat4 model, glm::mat4 view, glm::mat4 projection);
+
+private:
+	Shader();
+
+	unsigned int id;
+
+	void compile(std::string vertexCode, std::string fragmentCode);
+	void add(const char* code, GLenum type);
+	bool checkProgramStatus(GLenum status);
+	bool checkShaderStatus(unsigned int shader, GLenum status);
+	
+	static std::string readFileAsString(const char* path);
+};
